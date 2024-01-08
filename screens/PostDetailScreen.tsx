fefe52d1a0f2d193dd1daa892dc37152fa9a4872
Fakhreza SUparyadi <fakhreza.spy@gmail.com>
2024-01-08 08:01:07 +0700

    adding function to the app

--- screens/PostDetailScreen.tsx
+++ screens/PostDetailScreen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import {
   Button,
   Image,
@@ -19,9 +20,83 @@ import IconDownvoteInactive from '../assets/downvote_inactive.png';
 import IconShare from '../assets/share.png';
 import IconUpvoteActive from '../assets/upvote_active.png';
 import IconUpvoteInactive from '../assets/upvote_inactive.png';
+import {CommentData, PostDetailProps} from './MyType';
 
-function PostDetailScreen() {
-  const navigation = useNavigation();
+function PostDetailScreen({route, navigation}) {
+  const {
+    postContentData,
+    handleAddComment,
+    handleDownvote,
+    handleUpvote
+  } = route.params;
+  if (!postContentData) {
+    return <Text>Loading...</Text>; // or render some loading indicator
+  }
+  const {
+    id,
+    content,
+    upvotes,
+    comment,
+    didUpvotes,
+  } = postContentData;
+
+  const [commentInput, setCommentInput] = useState('')
+
+  const renderComment = (commentData:CommentData, index: number) => {
+    return (
+    <View
+    style={{
+      flexDirection: 'row',
+      minHeight: 72,
+      paddingVertical: 16,
+      paddingHorizontal: 24,
+    }}
+    key={index}
+    >
+    <Image
+      source={{
+        uri: 'https://picsum.photos/200',
+      }}
+      width={36}
+      height={36}
+      style={{borderRadius: 24, marginRight: 16}}
+    />
+    <View style={{width: '90%'}}>
+      <Text
+        style={{
+          fontWeight: '600',
+          fontSize: 12,
+          lineHeight: 14.52,
+          color: '#828282',
+        }}>
+        {commentData.createdBy}
+      </Text>
+      <Text style={{fontWeight: '400', fontSize: 16, lineHeight: 19.36}}>
+        {
+          commentData.content
+        }
+      </Text>
+    </View>
+  </View>
+    )
+  }
+
+  const handleComment = () => {
+    const newCommentData:CommentData = {
+      content: commentInput,
+      createdBy: 'Asep',
+      date: '22-10-2023'
+    }
+    handleAddComment(postContentData.id, newCommentData)
+    setCommentInput('')
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      setCommentInput('');
+    }, [route.params])
+  );
+
   return (
     <SafeAreaView>
       <ScrollView style={{marginBottom: 48}}>
@@ -51,23 +126,17 @@ function PostDetailScreen() {
             <View style={{marginLeft: 16}}>
               <Text
                 style={{fontWeight: '600', fontSize: 14, lineHeight: 16.94}}>
-                Usup Suparma
+                {postContentData?.postedBy ?? '-' }
               </Text>
               <Text style={{fontWeight: '400', fontSize: 12, lineHeight: 18}}>
-                Mar 27, 2023
+                {postContentData?.date ?? '-'}
               </Text>
             </View>
           </View>
           <View style={{height: 0.5, backgroundColor: '#C4C4C4'}} />
           <View>
             <Text style={{margin: 24}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              luctus in ipsum ac dictum. Integer et nunc ut tellus tinci,
-              consectetur adipiscing elit. Nulla luctus in ipsum ac dictum.
-              Integer et nunc ut tellus tinci, consectetur adipiscing elit.
-              Nulla luctus in ipsum ac dictum. Integer et nunc ut tellus tinci
-              Nulla luctus in ipsum ac dictum. Integer et nunc ut tellus tinci,
-              consectetur adipiscing elit. Nulla luctus in ipsum ac dictum.
+              {postContentData?.content ?? '-'}
             </Text>
             <Image
               source={{
@@ -106,7 +175,7 @@ function PostDetailScreen() {
                   marginHorizontal: 4,
                   textAlign: 'center',
                 }}>
-                0
+                { postContentData?.comment?.length }
               </Text>
             </View>
             <View
@@ -120,12 +189,12 @@ function PostDetailScreen() {
                 width={18}
                 style={{marginLeft: 22}}
               />
-              <Pressable onPress={() => console.log('downvote')}>
+              <Pressable onPress={() => handleDownvote(postContentData?.id)}>
                 <Image
                   source={IconDownvoteInactive}
                   height={18}
                   width={18}
-                  style={{marginLeft: 24}}
+                  style={{marginLeft: 24, backgroundColor: postContentData?.didUpvotes === 'downvotes' ? 'black' : undefined }}
                 />
               </Pressable>
               <Text
@@ -134,116 +203,27 @@ function PostDetailScreen() {
                   marginHorizontal: 11,
                   textAlign: 'center',
                 }}>
-                0
+                { postContentData.upvotes }
               </Text>
-              <Pressable onPress={() => console.log('upvote')}>
+              <Pressable onPress={() => handleUpvote(postContentData?.id)}>
                 <Image
                   source={IconUpvoteInactive}
                   height={18}
                   width={18}
-                  style={{marginRight: 22}}
+                  style={{marginLeft: 22, backgroundColor: postContentData?.didUpvotes === 'upvotes' ? 'black' : undefined }}
                 />
               </Pressable>
             </View>
           </View>
         </View>
         <View style={{height: 4, backgroundColor: '#C4C4C4'}} />
-        <View
-          style={{
-            flexDirection: 'row',
-            minHeight: 72,
-            paddingVertical: 16,
-            paddingHorizontal: 24,
-          }}>
-          <Image
-            source={{
-              uri: 'https://picsum.photos/200',
-            }}
-            width={36}
-            height={36}
-            style={{borderRadius: 24, marginRight: 16}}
-          />
-          <View style={{width: '90%'}}>
-            <Text
-              style={{
-                fontWeight: '600',
-                fontSize: 12,
-                lineHeight: 14.52,
-                color: '#828282',
-              }}>
-              Usup Suparma
-            </Text>
-            <Text style={{fontWeight: '400', fontSize: 16, lineHeight: 19.36}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              luctus in ipsum ac dictum. Integer et nunc ut tellus tinci,
-            </Text>
-          </View>
-        </View>
-        <View style={{height: 0.5, backgroundColor: '#C4C4C4'}} />
-        <View
-          style={{
-            flexDirection: 'row',
-            minHeight: 72,
-            paddingVertical: 16,
-            paddingHorizontal: 24,
-          }}>
-          <Image
-            source={{
-              uri: 'https://picsum.photos/200',
-            }}
-            width={36}
-            height={36}
-            style={{borderRadius: 24, marginRight: 16}}
-          />
-          <View style={{width: '90%'}}>
-            <Text
-              style={{
-                fontWeight: '600',
-                fontSize: 12,
-                lineHeight: 14.52,
-                color: '#828282',
-              }}>
-              Usup Suparma
-            </Text>
-            <Text style={{fontWeight: '400', fontSize: 16, lineHeight: 19.36}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              luctus in ipsum ac dictum. Integer et nunc ut tellus tinci,
-            </Text>
-          </View>
-        </View>
-        <View style={{height: 0.5, backgroundColor: '#C4C4C4'}} />
-        <View
-          style={{
-            flexDirection: 'row',
-            minHeight: 72,
-            paddingVertical: 16,
-            paddingHorizontal: 24,
-          }}>
-          <Image
-            source={{
-              uri: 'https://picsum.photos/200',
-            }}
-            width={36}
-            height={36}
-            style={{borderRadius: 24, marginRight: 16}}
-          />
-          <View style={{width: '90%'}}>
-            <Text
-              style={{
-                fontWeight: '600',
-                fontSize: 12,
-                lineHeight: 14.52,
-                color: '#828282',
-              }}>
-              Usup Suparma
-            </Text>
-            <Text style={{fontWeight: '400', fontSize: 16, lineHeight: 19.36}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              luctus in ipsum ac dictum. Integer et nunc ut tellus tinci,
-            </Text>
-          </View>
-        </View>
-        <View style={{height: 0.5, backgroundColor: '#C4C4C4'}} />
+        {
+          postContentData?.comment?.map((el:CommentData, index:number) => {
+            return(
+              renderComment(el, index)
+            )
+          })
+        }
       </ScrollView>
       <View
         style={{
@@ -257,8 +237,8 @@ function PostDetailScreen() {
           zIndex: 10,
         }}>
         <View style={{height: 0.5, backgroundColor: '#C4C4C4'}} />
-        <TextInput placeholder="Enter Comment" style={{flex: 1}} />
-        <Button title="Comment" onPress={() => console.log('comment')} />
+        <TextInput placeholder="Enter Comment" style={{flex: 1}} value={commentInput} onChangeText={(text) => setCommentInput(text)} />
+        <Button title="Comment" onPress={() => handleComment()} />
       </View>
     </SafeAreaView>
   );
